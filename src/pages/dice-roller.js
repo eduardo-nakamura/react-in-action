3f4b3acaa632dc3ad10841c0ca44a7ceaf25f91e
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-07-18 02:15:04 -0300

    responsivo

--- src/pages/dice-roller.js
+++ src/pages/dice-roller.js
@@ -1,19 +1,30 @@
 import React, {useState} from 'react';
 import '../App.scss';
-import { ReactComponent as d4 } from '../assets/d4.svg'
-
+import {FaRegQuestionCircle} from 'react-icons/fa'
+import {BsFillTrashFill} from 'react-icons/bs'
+import {IoIosAddCircle} from 'react-icons/io'
+// IoIosAddCircle
+import imgagemEscolhida from '../assets/d4.svg'
+import { ReactSVG } from 'react-svg'
+import diceFourImg from '../assets/d4.svg';
+import diceSixImg from '../assets/d6.svg';
+import diceEightImg from '../assets/d8.svg';
+import diceTenImg from '../assets/d10.svg';
+import diceTwelveImg from '../assets/d12.svg';
+import diceTwentyImg from '../assets/d20.svg';
+import diceHundredImg from '../assets/d100.svg';
 
 function DiceRoller() {
   const empty = {"diceNum": null, "diceQuant": null, "diceBonus": null}
-  const [diceOb,setDiceObj] = useState([empty])
+  const [diceOb,setDiceObj] = useState([])
   
-  const dice4 = {"diceNum": 4, "diceQuant": null, "diceBonus": null}
-  const dice6 = {"diceNum": 6, "diceQuant": null, "diceBonus": null}
-  const dice8 = {"diceNum": 8, "diceQuant": null, "diceBonus": null}
-  const dice10 = {"diceNum": 10, "diceQuant": null, "diceBonus": null}
-  const dice12 = {"diceNum": 12, "diceQuant": null, "diceBonus": null}
-  const dice20 = {"diceNum": 20, "diceQuant": null, "diceBonus": null}
-  const dice100 = {"diceNum": 100, "diceQuant": null, "diceBonus": null}
+  const dice4 = {"diceNum": 4, "diceQuant": 1, "diceBonus": 0}
+  const dice6 = {"diceNum": 6, "diceQuant": 1, "diceBonus": 0}
+  const dice8 = {"diceNum": 8, "diceQuant": 1, "diceBonus": 0}
+  const dice10 = {"diceNum": 10, "diceQuant": 1, "diceBonus": 0}
+  const dice12 = {"diceNum": 12, "diceQuant": 1, "diceBonus": 0}
+  const dice20 = {"diceNum": 20, "diceQuant":1, "diceBonus": 0}
+  const dice100 = {"diceNum": 100, "diceQuant": 1, "diceBonus": 0}
   let handleChangeAlt = (itemAlt, index) => {    
     let { name, value } = itemAlt.target;
     let newArr = [...diceOb]; 
@@ -22,7 +33,12 @@ function DiceRoller() {
   }  
 
   const [allDice,setAllDice] = useState([])
-
+  function deleteLine(index){
+    let newArr = [...diceOb]; 
+    console.log(newArr[index])
+    newArr.splice(index,1)
+    setDiceObj(newArr);
+  }
   
   return (
     <div className="container">
@@ -32,23 +48,42 @@ function DiceRoller() {
       </div>
     
       <div className="section">
-      <p onClick={() => setDiceObj(diceOb.concat(empty)) }>ee</p>
+     
       <div className="dices-type">
-        <img src={d4} alt="a"/>
-        <d4/>
+        <img onClick={() => setDiceObj(diceOb.concat(dice4)) } style={{width: "50px"}} src={diceFourImg} alt=""/>
+        <img onClick={() => setDiceObj(diceOb.concat(dice6)) } style={{width: "50px"}} src={diceSixImg} alt=""/>
+        <img onClick={() => setDiceObj(diceOb.concat(dice8)) } style={{width: "50px"}} src={diceEightImg} alt=""/>
+        <img onClick={() => setDiceObj(diceOb.concat(dice10)) } style={{width: "50px"}} src={diceTenImg} alt=""/>
+        <img onClick={() => setDiceObj(diceOb.concat(dice12)) } style={{width: "50px"}} src={diceTwelveImg} alt=""/>
+        <img onClick={() => setDiceObj(diceOb.concat(dice20)) } style={{width: "50px"}} src={diceTwentyImg} alt=""/>
+        <FaRegQuestionCircle style={{color: "white"}} size={50} onClick={() => setDiceObj(diceOb.concat(empty)) }/>
+        {/* <img onClick={() => setDiceObj(diceOb.concat(dice100)) } style={{width: "50px"}} src={diceHundredImg} alt=""/> */}
       </div>
           <table class="w3-table">
             <tr>
-              <th>Faces</th>
-              <th>Quantidade</th>
-              <th>Bonus</th>
+            <th>Quantidade:</th>
+              <th>Faces:</th>
+             
+              <th>Bonus:</th>
+              <th><IoIosAddCircle onClick={() => setDiceObj(diceOb.concat(empty)) }/></th>
             </tr>
             {
                 diceOb && diceOb.slice(0, 12).map((item, index) => {
                   return (
                     <tr>
+                     
                       <td>
-                    
+                        
+                      <input                           
+                          type="number"
+                          name="diceQuant"
+                          onChange={(item) => handleChangeAlt(item, index)}   
+                          value={diceOb[index].diceQuant}              
+                        />
+                       
+                        </td>
+                        <td>                    
+                       
                         <input                           
                           type="number"
                           name="diceNum"
@@ -57,20 +92,17 @@ function DiceRoller() {
                         />
                       </td>
                       <td>
-                      <input                           
-                          type="number"
-                          name="diceQuant"
-                          onChange={(item) => handleChangeAlt(item, index)}   
-                          value={diceOb[index].diceQuant}              
-                        />
-                        </td>
-                      <td>
+                      
                       <input                           
                           type="number"
                           name="diceBonus"
                           onChange={(item) => handleChangeAlt(item, index)}  
                           value={diceOb[index].diceBonus}                
                         />
+                        
+                       </td>
+                       <td>
+                       <BsFillTrashFill onClick={() => deleteLine(index)}/>
                        </td>
                     </tr>
                   )
