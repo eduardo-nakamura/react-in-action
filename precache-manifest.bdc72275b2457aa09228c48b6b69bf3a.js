b3eb62046441e1a97de73c696d0863d45a2b3e32
eduardo-nakamura <edu-nakamura@hotmail.com>
2020-07-18 19:21:25 -0300

    Updates

---
+++ precache-manifest.bdc72275b2457aa09228c48b6b69bf3a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c047e17b786e7054dc9ee91125de6b",
+    "url": "/react-in-action/index.html"
+  },
+  {
+    "revision": "e94a4d56a96e9ed1ad8d",
+    "url": "/react-in-action/static/css/main.0d45b747.chunk.css"
+  },
+  {
+    "revision": "5ffb62ce1979a0f2686a",
+    "url": "/react-in-action/static/js/2.7e14693a.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/react-in-action/static/js/2.7e14693a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94a4d56a96e9ed1ad8d",
+    "url": "/react-in-action/static/js/main.6153ee85.chunk.js"
+  },
+  {
+    "revision": "a0aede214010736cdecc",
+    "url": "/react-in-action/static/js/runtime-main.86b5e528.js"
+  },
+  {
+    "revision": "c480e6fbcc3b0a4db4ce3bd823edf749",
+    "url": "/react-in-action/static/media/custom.c480e6fb.svg"
+  },
+  {
+    "revision": "6b89f59dceeb6488edb9aac673d9197b",
+    "url": "/react-in-action/static/media/d4.6b89f59d.svg"
+  },
+  {
+    "revision": "43189fd4b23a22e02055e7b26ee0dd9f",
+    "url": "/react-in-action/static/media/dice10.43189fd4.svg"
+  },
+  {
+    "revision": "71e7a6e6ae5bc65736690c67e229e2a3",
+    "url": "/react-in-action/static/media/dice100.71e7a6e6.svg"
+  },
+  {
+    "revision": "612bc23272ef2145f0f8ce67ca0a45fe",
+    "url": "/react-in-action/static/media/dice12.612bc232.svg"
+  },
+  {
+    "revision": "a6407763c3cce723ba69b26dbfa96c19",
+    "url": "/react-in-action/static/media/dice20.a6407763.svg"
+  },
+  {
+    "revision": "0b5e9ddd93e28b80fd37a18df4d75f1f",
+    "url": "/react-in-action/static/media/dice4.0b5e9ddd.svg"
+  },
+  {
+    "revision": "29b34da3c3e837fc8a85abeda40732d1",
+    "url": "/react-in-action/static/media/dice6.29b34da3.svg"
+  },
+  {
+    "revision": "868649f0e4177ed012c904fea7711ef8",
+    "url": "/react-in-action/static/media/dice8.868649f0.svg"
+  }
+]);
